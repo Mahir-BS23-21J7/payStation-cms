09adf8dca1bfec8eff06b7fc9770d135ddb651a2
Mahir <Mahir.Shahrier@Brainstation-23.com>
2023-01-20 09:47:25 +0600

    Progress: routes modified

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\SubscriptionHistoryController;
+use App\Http\Controllers\SubscriptionController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -15,6 +17,8 @@ use Inertia\Inertia;
 |
 */
 
+require __DIR__ . '/auth.php';
+
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
@@ -24,6 +28,7 @@ Route::get('/', function () {
     ]);
 })->name('landing');
 
+// Dashboard
 Route::get('/dashboard', function () {
     return Inertia::render('User/Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -32,6 +37,10 @@ Route::get('/admin/dashboard', function () {
     return Inertia::render('Admin/DashboardAdmin');
 })->middleware(['auth:admin', 'verified'])->name('admin.dashboard');
 
-require __DIR__.'/auth.php';
 
-require __DIR__.'/subscription.php';
+// Subscription
+Route::group(['prefix' => 'subscription'], function () {
+    Route::get('purchase', [SubscriptionController::class, 'purchaseSubscription'])->middleware('auth')->name('subscription.purchase');
+    Route::get('all-plans', [SubscriptionController::class, 'showSubscriptionPlans'])->name('subscription.plans');
+    Route::get('user-history', [SubscriptionHistoryController::class, 'showUserSubscriptionHistory'])->middleware('auth')->name('subscription.user.history');
+});
